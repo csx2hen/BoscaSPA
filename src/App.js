0aac841c3d255708eb9a5c0c866026ad03e70a91
csx2hen <csx2hen@gmail.com>
2020-04-28 17:17:38 +0800

    Config aws-amplify(Auth).

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
+import awsConfig from './config/awsConfig';
+
+Amplify.configure(awsConfig);
 
 const App = () => {
   return (
     <div>
+      My App
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/config/awsConfig.js
@@ -0,0 +1,9 @@
+const awsConfig = {
+  aws_user_pools: 'enable',
+  aws_user_pools_id: 'us-west-2_oAS7boRtM',
+  aws_user_pools_mfa_type: 'OFF',
+  aws_user_pools_web_client_id: '1sod7oaeti4l10dqqe6aivdttu',
+  aws_user_settings: 'enable',
+};
+
+export default awsConfig;
