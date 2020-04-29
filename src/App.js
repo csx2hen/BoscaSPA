92ac9db965e305286041528496d4a35259448c4a
csx2hen <csx2hen@gmail.com>
2020-04-29 12:17:07 +0800

    Add router to App.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Amplify from 'aws-amplify';
 import awsConfig from './config/awsConfig';
-import SignIn from './containers/UserAuth/SignIn/SignIn';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './containers/Home/Home';
+import UserAuth from './containers/UserAuth/UserAuth';
 
 Amplify.configure(awsConfig);
 
 const App = () => {
   return (
-    <div>
-      My App
-      <SignIn/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={Home}/>
+        <Route path="/auth" component={UserAuth}/>
+      </div>
+    </BrowserRouter>
+
   );
 };
 
