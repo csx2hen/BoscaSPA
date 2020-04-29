578bc9208724e1bedf58d98c7ee2bb08e24d6a5b
csx2hen <csx2hen@gmail.com>
2020-04-29 12:50:40 +0800

    Add SignUp without actions.

--- src/containers/UserAuth/SignUp/SignUp.js
+++ src/containers/UserAuth/SignUp/SignUp.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import {formContentConfig, formItemLayout, tailFormItemLayout} from './SignUpFormConfig';
+import {Button, Form, Input} from 'antd';
+import CenterBox from '../../../components/UI/CenterBox/CenterBox';
 
 const SignUp = (props) => {
 
+  const formItems = formContentConfig.map(e => {
+    return (
+      <Form.Item {...formItemLayout}
+                 key={e.name}
+                 name={e.name}
+                 rules={e.rules}
+                 label={e.label}
+                 hasFeedback={e.hasFeedback}
+                 dependencies={e.dependencies}>
+        <Input type={e.type}/>
+      </Form.Item>
+    );
+  });
+
   return (
-    <div>
-      SignUp
-    </div>
+    <CenterBox title="Create a new account" width={8}>
+      <Form>
+        {formItems}
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Create account
+          </Button>
+        </Form.Item>
+      </Form>
+    </CenterBox>
   );
 };
 
---
+++ src/containers/UserAuth/SignUp/SignUpFormConfig.js
@@ -0,0 +1,137 @@
+const formContentConfig = [
+  {
+    name: 'email',
+    label: 'E-mail',
+    rules: [
+      {
+        required: true,
+        message: 'Please input your E-mail address.',
+      },
+      {
+        type: 'email',
+        message: 'The input is not a valid E-mail address.',
+      },
+    ],
+  },
+  {
+    name: 'name',
+    label: 'Name',
+    rules: [
+      {
+        required: true,
+        message: 'Please input your preferred username.',
+      },
+    ],
+  },
+  {
+    name: 'password',
+    type: 'password',
+    label: 'Password',
+    hasFeedback: true,
+    rules: [
+      {
+        required: true,
+        message: 'Please input your password',
+      },
+      {
+        validator: (rule, value) => {
+          const format = /[a-z]/;
+          if (value && !format.test(value)) {
+            return Promise.reject('No lowercase letters');
+          }
+          return Promise.resolve();
+        },
+      },
+      {
+        validator: (rule, value) => {
+          const format = /[A-Z]/;
+          if (value && !format.test(value)) {
+            return Promise.reject('No uppercase letters');
+          }
+          return Promise.resolve();
+        },
+      },
+      {
+        validator: (rule, value) => {
+          const format = /[0-9]/;
+          if (value && !format.test(value)) {
+            return Promise.reject('No numbers');
+          }
+          return Promise.resolve();
+        },
+      },
+      {
+        validator: (rule, value) => {
+          const format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+          if (value && !format.test(value)) {
+            return Promise.reject('No special characters');
+          }
+          return Promise.resolve();
+        },
+      },
+      {
+        validator: (rule, value) => {
+          if (value && value.length < 8) {
+            return Promise.reject('Less than 8 characters');
+          }
+          return Promise.resolve();
+        },
+      },
+    ],
+  },
+  {
+    name: 'confirm',
+    type: 'password',
+    label: 'Confirm Password',
+    hasFeedback: true,
+    dependencies: ['password'],
+    rules: [
+      {
+        required: true,
+        message: 'Please confirm your password',
+      },
+      ({getFieldValue}) => ({
+        validator(rule, value) {
+          if (!value || getFieldValue('password') === value) {
+            return Promise.resolve();
+          }
+          return Promise.reject('The two passwords do not match');
+        },
+      }),
+    ],
+  },
+];
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+
+export {formContentConfig, formItemLayout, tailFormItemLayout};
