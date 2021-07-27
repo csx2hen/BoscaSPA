9a189f90c3aadf00367c6e52a5cd67fed0da0eb2
csx2hen <csx2hen@gmail.com>
2021-07-27 17:13:50 +0800

    Remove AWS config.

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -1,17 +1,16 @@
 const awsConfig = {
   Auth: {
-    identityPoolId: 'us-west-2:c7666fcf-0976-4a22-b595-0949ebb5545d',
-    region: 'us-west-2',
-    userPoolId: 'us-west-2_oAS7boRtM',
-    userPoolWebClientId: '1sod7oaeti4l10dqqe6aivdttu',
+    identityPoolId: '',
+    region: '',
+    userPoolId: '',
+    userPoolWebClientId: '',
   },
   Storage: {
     AWSS3: {
-      bucket: 'bosca-user-files',
-      region: 'us-west-2',
+      bucket: '',
+      region: '',
     },
   },
-
 };
 
 export default awsConfig;
